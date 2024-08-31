f2a9681120da2887953937ac68889f7e43456d43
mohamedNaser1212 <nasergamal33e@gmail.com>
2024-08-31 22:18:18 +0300

    auths and cart connection check done

--- lib/core/utils/widgets/reusable_widgets_manager/toast_widget.dart
+++ lib/core/utils/widgets/reusable_widgets_manager/toast_widget.dart
@@ -16,7 +16,7 @@ class ToastWidget extends StatelessWidget {
     Fluttertoast.showToast(
       msg: message,
       toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.CENTER,
+      gravity: ToastGravity.BOTTOM,
       timeInSecForIosWeb: 1,
       backgroundColor: isError ? Colors.red : Colors.green,
       textColor: Colors.white,
