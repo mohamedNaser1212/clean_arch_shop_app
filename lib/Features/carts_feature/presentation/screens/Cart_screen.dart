7900ef3e11762c56cacab5b1eef7ac2dbf0b7770
mohamedNaser1212 <nasergamal33e@gmail.com>
2024-09-22 08:49:41 +0300

    solve the cart problem

--- lib/Features/carts_feature/presentation/screens/Cart_screen.dart
+++ lib/Features/carts_feature/presentation/screens/Cart_screen.dart
@@ -14,39 +14,43 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocListener<ToggleCartCubit, ToggleCartState>(
-      listener: (context, state) {
-        if (state is ToggleCartItemsErrorState) {
-          showToast(
-            message: state.error,
-            isError: true,
-          );
-        } else if (state is ChangeCartListErrorState) {
-          showToast(
-            message: state.error,
-            isError: true,
-          );
-        } else if (state is ToggleCartSuccessState ||
-            state is ChangeCartListSuccessState) {
-          CartsCubit.get(context).getCartItems();
-        }
-      },
+      listener: (context, state) => _listener(context, state),
       child: BlocBuilder<CartsCubit, CartsState>(
-        builder: (context, state) {
-          if (state is GetCartItemsSuccessState) {
-            return CartScreenContent(
-              state: state,
-            );
-          } else if (state is GetCartItemsErrorState) {
-            return Center(
-              child: Text(state.error),
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+        builder: (context, state) => _cartScreenBuilder(context, state),
       ),
     );
+  }
+
+  void _listener(BuildContext context, ToggleCartState state) {
+    if (state is ToggleCartItemsErrorState) {
+      showToast(
+        message: state.error,
+        isError: true,
+      );
+    } else if (state is ChangeCartListErrorState) {
+      showToast(
+        message: state.error,
+        isError: true,
+      );
+    } else if (state is ToggleCartSuccessState ||
+        state is ChangeCartListSuccessState) {
+      CartsCubit.get(context).getCartItems();
+    }
+  }
+
+  Widget _cartScreenBuilder(BuildContext context, CartsState state) {
+    if (state is GetCartItemsSuccessState) {
+      return CartScreenContent(
+        state: state,
+      );
+    } else if (state is GetCartItemsErrorState) {
+      return Center(
+        child: Text(state.error),
+      );
+    } else {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
   }
 }
